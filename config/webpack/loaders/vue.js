9f3d6a0858e2fdf732df44827ff862b06e9575b2
Felipe <feliara8@gmail.com>
2020-10-22 00:48:41 -0300

    Try with css

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -5,7 +5,8 @@ module.exports = {
 	options: {
 	  loaders: {
 	    'scss': 'vue-style-loader!css-loader!sass-loader',
-	    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+	    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+	    'css': 'vue-style-loader!css-loader!sass-loader',
 	  }
 	}
   }]
